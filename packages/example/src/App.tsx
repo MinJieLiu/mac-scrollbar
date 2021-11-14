db978cf03faa607c3b5fd8d646a3932449b94c9f
MinJieLiu <liumingyi_1@163.com>
2021-11-14 22:26:43 +0800

    feat: demo

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -1,65 +1,123 @@
 import React from 'react';
 import { MacScrollbar } from 'mac-scrollbar';
+import styled from 'styled-components';
 import 'mac-scrollbar/dist/style.css';
-import './App.css';
+
+const Main = styled.div`
+  padding: 10px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 20px;
+`;
+
+const NoWrap = styled.div`
+  white-space: nowrap;
+`;
+
+const WrapperAutoScrollbar = styled(MacScrollbar)`
+  flex: 1;
+  height: 500px;
+  padding: 20px;
+  background: whitesmoke;
+`;
+
+const LightThemeScrollbar = styled(MacScrollbar)`
+  padding: 20px;
+  background: whitesmoke;
+`;
+
+const DarkThemeScrollbar = styled(MacScrollbar)`
+  padding: 20px;
+  background: darkslategray;
+  color: white;
+`;
+
+const baseIntro = (
+  <>
+    <p>React is a JavaScript library for building user interfaces.</p>
+
+    <p>
+      Declarative: React makes it painless to create interactive UIs. Design simple views for each
+      state in your application, and React will efficiently update and render just the right
+      components when your data changes. Declarative views make your code more predictable, simpler
+      to understand, and easier to debug.
+    </p>
+    <p>
+      Component-Based: Build encapsulated components that manage their own state, then compose them
+      to make complex UIs. Since component logic is written in JavaScript instead of templates, you
+      can easily pass rich data through your app and keep state out of the DOM.
+    </p>
+    <p>
+      Learn Once, Write Anywhere: We don't make assumptions about the rest of your technology stack,
+      so you can develop new features in React without rewriting existing code. React can also
+      render on the server using Node and power mobile apps using React Native.
+    </p>
+
+    <p>Learn how to use React in your own project.</p>
+
+    <h3>Installation</h3>
+    <p>
+      React has been designed for gradual adoption from the start, and you can use as little or as
+      much React as you need:
+    </p>
+
+    <p>Use Online Playgrounds to get a taste of React.</p>
+    <p>Add React to a Website as a script tag in one minute.</p>
+    <p>Create a New React App if you're looking for a powerful JavaScript toolchain.</p>
+    <p>You can use React as a script tag from a CDN, or as a react package on npm.</p>
+
+    <h3>Documentation</h3>
+    <p>You can find the React documentation on the website.</p>
+
+    <p>Check out the Getting Started page for a quick overview.</p>
+
+    <p>The documentation is divided into several sections:</p>
+
+    <p>Tutorial</p>
+    <p>Main Concepts</p>
+    <p>Advanced Guides</p>
+    <p>API Reference</p>
+    <p>Where to Get Support</p>
+    <p>Contributing Guide</p>
+    <p>You can improve it by sending pull requests to this repository.</p>
+  </>
+);
+
+const baseIntro2 = (
+  <>
+    React is a JavaScript library for building user interfaces. Declarative: React makes it painless
+    to create interactive UIs. Design simple views for each state in your application, and React
+    will efficiently update and render just the right components when your data changes. Declarative
+    views make your code more predictable, simpler to understand, and easier to debug.
+    Component-Based: Build encapsulated components that manage their own state, then compose them to
+    make complex UIs. Since component logic is written in JavaScript instead of templates, you can
+    easily pass rich data through your app and keep state out of the DOM. Learn Once, Write
+    Anywhere: We don't make assumptions about the rest of your technology stack, so you can develop
+    new features in React without rewriting existing code. React can also render on the server using
+    Node and power mobile apps using React Native.
+  </>
+);
 
 function App() {
   return (
-    <MacScrollbar className="App">
-      <p>Hello Vite + React!</p>
-      <p style={{ whiteSpace: 'nowrap' }}>
-        Hello Vite + React!Hello Vite + React!Hello Vite + React!Hello Vite + React!Hello Vite +
-        React!Hello Vite + React!Hello Vite + React!Hello Vite + React!Hello Vite + React!Hello Vite
-        + React!Hello Vite + React!
-      </p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-      <p>Hello Vite + React!</p>
-    </MacScrollbar>
+    <>
+      <Main>
+        <WrapperAutoScrollbar>{baseIntro}</WrapperAutoScrollbar>
+        <WrapperAutoScrollbar>
+          <NoWrap>{baseIntro}</NoWrap>
+        </WrapperAutoScrollbar>
+      </Main>
+      <Main>
+        <LightThemeScrollbar suppressScrollY>
+          <NoWrap>{baseIntro2}</NoWrap>
+        </LightThemeScrollbar>
+      </Main>
+      <Main>
+        <DarkThemeScrollbar theme="dark">
+          <NoWrap>{baseIntro2}</NoWrap>
+        </DarkThemeScrollbar>
+      </Main>
+    </>
   );
 }
 
